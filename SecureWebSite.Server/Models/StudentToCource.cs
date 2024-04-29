b8a842151f7f1b02366c02f1c3545a270cda89f0
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-04-29 11:00:56 +0530

    feedBack AIis

---
+++ SecureWebSite.Server/Models/StudentToCource.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SecureWebSite.Server.Models
+{
+    public class StudentToCourse
+
+    {
+        [Key]
+        public int id { get; set; }
+        public string stname { get; set; }
+        public string course
+        {
+            get; set;
+        }
+
+
+    }
+}
---
+++ SecureWebSite.Server/Models/StudentToLecture.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SecureWebSite.Server.Models
+{
+    public class StudentToLecture
+    {
+        [Key]
+        public int id { get; set; }
+        public string stname { get; set; }
+        public string Lecture
+        {
+            get; set;
+        }
+    }
+}
