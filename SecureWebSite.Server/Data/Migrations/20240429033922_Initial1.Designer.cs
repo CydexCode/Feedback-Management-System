2fed61bb368bea2cfbc750fedaa9de991bc87f84
ManurangaJay <manurangajayawardhana@gmail.com>
2024-04-29 09:12:56 +0530

    Login registration API connect with back-end

--- SecureWebSite.Server/Data/Migrations/20240429033922_Initial1.Designer.cs
+++ SecureWebSite.Server/Data/Migrations/20240429033922_Initial1.Designer.cs
@@ -12,8 +12,8 @@ using SecureWebSite.Server.Data;
 namespace SecureWebSite.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240318204827_Initial")]
-    partial class Initial
+    [Migration("20240429033922_Initial1")]
+    partial class Initial1
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
